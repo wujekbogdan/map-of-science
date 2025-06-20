b114d0494e056d3e62d05fc6f0f0e928487892d1
wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
2025-06-20 14:42:17 +0200

    chore: move the Shape component into a separate file

--- src/apps/web/src/components/Map/DataPoints/DataPoints.tsx
+++ src/apps/web/src/components/Map/DataPoints/DataPoints.tsx
@@ -11,9 +11,10 @@ import { useState } from "react";
 import { createPortal } from "react-dom";
 import { useShallow } from "zustand/react/shallow";
 import { Concept, DataPoint as Point } from "../../../api/model";
-import { useArticleStore, useStore, RGB } from "../../../store.ts";
+import { useArticleStore, useStore } from "../../../store.ts";
 import { DataPointDetails } from "./DataPointDetails.tsx";
 import css from "./DataPoints.module.scss";
+import Shape from "./Shape.tsx";
 
 type Mode = "growth" | "regular";
 
@@ -33,68 +34,7 @@ const getLevelByArticlesCount = (articlesCount: number) => {
   return 6;
 };
 
-type ShapeOptions = {
-  point: {
-    growthRating: number;
-    x: number;
-    y: number;
-  };
-  uniformStyle: boolean;
-  mode: Mode;
-  level: 1 | 2 | 3 | 4 | 5 | 6;
-  growthRatingColors: {
-    start: RGB;
-    middle: RGB;
-    end: RGB;
-  };
-};
-
 const classes = (classList: string[]) => classList.join(" ");
-
-const lerp = (a: number, b: number, t: number) => a + (b - a) * t;
-
-const interpolateColor = (c1: RGB, c2: RGB, t: number): RGB => ({
-  r: Math.round(lerp(c1.r, c2.r, t)),
-  g: Math.round(lerp(c1.g, c2.g, t)),
-  b: Math.round(lerp(c1.b, c2.b, t)),
-});
-
-const getGradientColor = (
-  growthRating: number,
-  colors: {
-    start: RGB;
-    middle: RGB;
-    end: RGB;
-  },
-) => {
-  const t = growthRating / 100;
-  const [from, to, localT] =
-    t < 0.5
-      ? [colors.start, colors.middle, t * 2]
-      : [colors.middle, colors.end, (t - 0.5) * 2];
-
-  const rgb = interpolateColor(from, to, localT);
-  return `rgb(${rgb.r},${rgb.g},${rgb.b})`;
-};
-
-const Shape = (options: ShapeOptions) => {
-  const { point, uniformStyle, mode, growthRatingColors, level } = options;
-  const { x, y } = point;
-  const colorClasses =
-    mode === "regular" ? [css.outline, css.fill] : css.outline;
-  const sizeClass = css[`level-${level.toString()}`];
-  const classList = uniformStyle
-    ? [css.circle, ...colorClasses, sizeClass, css.searchResults]
-    : [css.circle, ...colorClasses, sizeClass];
-
-  // TODO: It might be more efficient to simply precalculate 100 CSS classes with SCSS
-  const style =
-    mode === "growth"
-      ? { fill: getGradientColor(point.growthRating, growthRatingColors) }
-      : undefined;
-
-  return <circle className={classes(classList)} cx={x} cy={y} style={style} />;
-};
 
 export const DataPoints = ({ points, concepts, uniformStyle, mode }: Props) => {
   const [growthRatingColors] = useStore(
