be68559845fda6c2f8c31e5f8ada57f9650471d7
wujekbogdan <wujekbogdan@users.noreply.github.com>
2025-07-05 21:17:52 +0200

    chore: add logo (#107)
    * chore: add logo
    
    * chore: changelog update
    
    ---------
    
    Co-authored-by: wujekbogdan <533954+wujekbogdan@users.noreply.github.com>

---
+++ .changeset/fresh-taxis-see.md
@@ -0,0 +1,5 @@
+---
+"@map-of-science/web": minor
+---
+
+Add logo
---
+++ src/apps/web/src/components/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import LogoSvg from "./logo.svg?react";
+
+const Logo = styled(LogoSvg)`
+  width: 200px;
+  height: auto;
+  display: block;
+  padding: 12px;
+`;
+
+export default Logo;
