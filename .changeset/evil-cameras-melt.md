f1316a391ae86fe12d71535d12ece98e8cd88a6a
wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
2025-12-29 10:13:29 +0100

    chore: replace p-queue with bottleneck to evenly distribute calls

--- .changeset/evil-cameras-melt.md
+++ .changeset/evil-cameras-melt.md
@@ -2,4 +2,4 @@
 "@map-of-science/rate-limiter": minor
 ---
 
-Add p-queue based rate limiting wrapper for async functions.
+Add rate limiting wrapper for async functions.
--- src/packages/rate-limiter/src/rateLimiter.ts
+++ src/packages/rate-limiter/src/rateLimiter.ts
@@ -1,15 +1,15 @@
-import PQueue from "p-queue";
+import Bottleneck from "bottleneck";
 
 export const createRateLimitedFunction = <TArgs extends unknown[], TReturn>(
   fn: (...args: TArgs) => Promise<TReturn>,
   requestsPerMinute = 10,
 ) => {
-  const queue = new PQueue({
-    interval: 60_000, // 1 minute in milliseconds
-    intervalCap: requestsPerMinute,
+  const limiter = new Bottleneck({
+    maxConcurrent: 1,
+    minTime: Math.ceil(60_000 / requestsPerMinute),
   });
 
   return (...args: TArgs): Promise<TReturn> => {
-    return queue.add(() => fn(...args));
+    return limiter.schedule(() => fn(...args));
   };
 };
