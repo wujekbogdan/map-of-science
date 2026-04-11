5db38bff35ecaaf61dafd263ac69716f75f5401b
wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
2026-04-11 11:50:22 +0200

    feat(atlas): add createSearch factory and package entry point

---
+++ src/packages/atlas/src/index.ts
@@ -0,0 +1,29 @@
+export {
+  bboxSchema,
+  clusterInputSchema,
+  clusterSchema,
+} from "./clusters/clusters.js";
+export type {
+  BBox,
+  Cluster,
+  ClusterInput,
+  ClusterMatch,
+  ClusterRepository,
+} from "./clusters/clusters.js";
+
+export { areaSchema } from "./areas/areas.js";
+export type { Area, AreaRepository } from "./areas/areas.js";
+
+export {
+  contentItemSchema,
+  entityRefSchema,
+  youtubeSegmentContentSchema,
+} from "./content/content.js";
+export type {
+  ContentItem,
+  ContentRepository,
+  EntityRef,
+} from "./content/content.js";
+
+export { createSearch } from "./search/search.js";
+export type { EmbedQuery } from "./search/search.js";
